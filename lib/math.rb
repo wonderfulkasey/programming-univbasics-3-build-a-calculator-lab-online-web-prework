6b0c1fd3bb7d8f1ed843738b5f43a7682e5f9542
Kasey Arnold <kaseyarnold15@gmail.com>
2019-12-23 02:28:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
-def addition(num1, num2)
-
+def addition(5, 4)
+5 + 4 = 9
 end
 
-def subtraction(num1, num2)
-
+def subtraction(10, 5)
+10 - 5 = 5
 end
 
-def division(num1, num2)
-
+def division(50, 2)
+50 / 2 = 25
 end
 
-def multiplication(num1, num2)
-
+def multiplication(4, 30)
+4 * 30 = 120
 end
 
-def modulo(num1, num2)
-
+def modulo(34, 5)
+34 % 5 = 4
 end
 
-def square_root(num)
-
+def square_root(81)
+Math.sqrt(9) = 9
 end
